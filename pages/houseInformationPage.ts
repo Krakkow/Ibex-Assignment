1336b074d6ae3262df1cb369b865640cf6344c9c
Or Kowalsky <okowalsky@gmail.com>
2025-03-25 17:41:32 +0200

    adding more locators to houseInformationPage.ts

--- pages/houseInformationPage.ts
+++ pages/houseInformationPage.ts
@@ -4,6 +4,9 @@ export class ListingInformationPage {
   constructor(private newTab: Page) {}
   private popupWindow = this.newTab.getByTestId("modal-container");
   private popupCloseButton = this.newTab.locator('button[aria-label="Close"]');
+  private locationTitle = this.newTab.locator('h2:has-text("Entire")');
+  private checkInDate = this.newTab.locator("label").filter({ hasText: "Check-in" }).locator("div").nth(2);
+  private checkoutDate = this.newTab.locator("label").filter({ hasText: "Checkout" }).locator("div").nth(2);
 
   async checkIfPopupExists(): Promise<boolean> {
     const isVisible = await this.popupWindow.isVisible();
